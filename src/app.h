0eb208e7119194b560231ca527e887ed6c0e1f7f
Andy <xandyx@riseup.net>
2021-08-03 18:04:08 +0200

    Added setting download URL at compile time

--- src/app.h
+++ src/app.h
@@ -246,7 +246,12 @@ private:
     const QString m_groupsRequestUrl = QStringLiteral("https://radar.squat.net/api/1.1/search/groups.json?fields[]=uuid&limit=1");
     const QString m_cityRequestUrlBase = QStringLiteral("https://radar.squat.net/api/1.2/search/groups.json?fields[]=uuid&limit=1&facets[country][]=%1");
     //Locations in city: https://radar.squat.net/api/1.2/search/location.json?facets[country][]=DE&facets[locality][]=Berlin&fields[]=directions&fields[]=title&fields[]=address&fields[]=uuid&fields[]=map
-    const QString m_downloadLink = QStringLiteral("https://0xacab.org/xandyx/radar-app/-/jobs/213170/artifacts/raw/build-android/radar-app-armeabi-v7a/build/outputs/apk/debug/radar-app-armeabi-v7a-debug.apk");
+    const QString m_downloadLink = 
+#ifdef APK_DOWNLOAD_URL
+	    QStringLiteral(APK_DOWNLOAD_URL);
+#else
+	    QStringLiteral("https://0xacab.org/xandyx/radar-app/-/jobs/213170/artifacts/raw/build-android/radar-app-armeabi-v7a/build/outputs/apk/debug/radar-app-armeabi-v7a-debug.apk");
+#endif
 
     const QString m_settingsCityKey = QStringLiteral("filter/city");
     const QString m_settingsCountryKey = QStringLiteral("filter/country");
