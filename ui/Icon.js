b33238ceb44463d7823c8d5705f647e7d1fdac26
Andy <xandyx@riseup.net>
2021-08-27 15:24:42 +0200

    Update copyright headers for QML sources

--- ui/Icon.js
+++ ui/Icon.js
@@ -1,3 +1,23 @@
+/*
+ *   Icon.js
+ *
+ *   Copyright (c) 2021 Andy Ex
+ *
+ *   This file is part of Radar-App.
+ *
+ *   Radar-App is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Radar-App is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Radar-App.  If not, see <https://www.gnu.org/licenses/>.
+ */
 var Icon = {
     "accessPoint": String.fromCodePoint(0xf0003),
     "calendar": String.fromCodePoint(0xf00ED),
